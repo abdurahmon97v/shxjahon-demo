61890317499a5bcb4b7d5195a7ceadd1413d9a6b
abdurahmon97v <abdurahmon97v@gmail.com>
2024-12-17 15:41:33 +0500

    bug

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Shoxjahon",
   description: "0 dan 6000$ gacha chiqish sirlari Treydrlikdan frontend emas",
   openGraph: {
-    images: "/images/money.png",
+    images: ["/images/money.png"],
   },
 };
 
